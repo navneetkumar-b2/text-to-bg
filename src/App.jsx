3811a8553884b2571ae605e97ec3c01f0a29759d
Navneet Kumar <navneetkumar.html@gmail.com>
2024-01-08 16:29:55 +0530

    change text color also
    functionality to change text color along with background color added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
-
 function App() {
   let [color, setColor] = useState('cyan');
+  let [txtclr, settxtclr] = useState('black');
   let showcolor=(n)=>{
     setColor(n)
     // alert(color)
   }
+  let showTxtclr=(clr)=>{
+    settxtclr(clr)
+  }
+
   return (
     <>
-      <h1  style={{backgroundColor : `${color}`}}>Set My Background color and Text color</h1>
-      <h2>text to background</h2>
+      <h1  style={{backgroundColor : `${color}`,color:`${txtclr}`}}>Set My Background color and Text color</h1>
+      <p>enter bg color here</p>
       <input type="text" placeholder="input color" value={color}
       onChange={(e)=>{showcolor(e.target.value)}}
+      >
+      </input>
+      <br/>
+
+        <p>enter text color here</p>
+      <input type="text" placeholder="input color" value={txtclr}
+      onChange={(e)=>{showTxtclr(e.target.value)}}
       >
       </input>
     </>
